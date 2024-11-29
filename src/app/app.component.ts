77bf7eccfcadf6a3a98b5b8c40097feee36376fe
Fadi Serapian <fadi.serapian@gmail.com>
2024-11-28 20:30:05 -0500

    Init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TodosComponent } from './todos/todos.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [TodosComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'todo-signals';
+    title = 'todo-signals';
 }
---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@Component({
+    selector: 'app-todos',
+    templateUrl: './todos.component.html',
+    imports: [HeaderComponent, MainComponent, FooterComponent]
+})
+export class TodosComponent {
+}

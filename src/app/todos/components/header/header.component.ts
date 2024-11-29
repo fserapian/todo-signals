383dd44477cd5fde3faa85cfd61f41f14ff9c475
Fadi Serapian <fadi.serapian@gmail.com>
2024-11-28 21:04:09 -0500

    Render todos in main and add a todo to the todos list in service

--- src/app/todos/components/header/header.component.ts
+++ src/app/todos/components/header/header.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TodosService } from '../../services/todos.service';
 
 @Component({
     selector: 'app-todos-header',
     templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+    text = ''
+    todosService = inject(TodosService);
+
+    changeText(e: Event) {
+        const target = e.target as HTMLInputElement;
+        this.text = target.value;
+    }
+
+    addTodo() {
+        this.todosService.addTodo(this.text);
+        this.text = '';
+    }
 }
--- src/app/todos/components/main/main.component.ts
+++ src/app/todos/components/main/main.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TodosService } from '../../services/todos.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-todos-main',
     templateUrl: './main.component.html',
+    imports: [CommonModule]
 })
 export class MainComponent {
+    todosService = inject(TodosService);
 }
